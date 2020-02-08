51f379f4df215f68cba6253ef2b54bef0d847f1e
Mark Harley <2378551h@student.gla.ac.uk>
2020-02-08 13:55:33 +0000

    Chapter 5 acessed Admin page

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -16,7 +16,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rango',
 ]
 
 MIDDLEWARE = [
@@ -127,4 +129,5 @@ STATIC_URL = '/static/'
 # Media Files
 
 MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
+
